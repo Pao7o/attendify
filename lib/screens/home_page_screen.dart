8b840b840c6cd2991104cf38541a66c1a6c6b7a1
Pao7o <92690817+Pao7o@users.noreply.github.com>
2022-11-07 19:04:11 +0100

    Daily push baby

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -65,7 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) => const LocationMap(),
+                                builder: (context) => ChatPage(),
                               ),
                             );
                           },
--- lib/screens/location_map.dart
+++ lib/screens/location_map.dart
@@ -1,107 +1,231 @@
-import 'package:attendify/screens/search_location.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:attendify/common/app_colors.dart';
-import 'package:attendify/common/common_widget.dart';
-import 'package:attendify/common/image_path.dart';
-import 'package:attendify/common/strings.dart';
-import 'package:get/get.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../responsive/responsive_flutter.dart';
-import 'edit_profile_screen.dart';
-import 'location_controller.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:mime/mime.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
-class LocationMap extends StatefulWidget {
 
 
-  const LocationMap({Key? key}) : super(key: key);
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
 
   @override
-  State<LocationMap> createState() => _LocationMapState();
+  State<ChatPage> createState() => _ChatPageState();
 }
 
-class _LocationMapState extends State<LocationMap> {
-  late CameraPosition _cameraPosition;
+class _ChatPageState extends State<ChatPage> {
+  List<types.Message> _messages = [];
+  final _user = const types.User(
+    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
+  );
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _cameraPosition=CameraPosition(target: LatLng(
-        45.521563,-122.677433
-    ), zoom: 17);
+    _loadMessages();
   }
-  late GoogleMapController _mapController;
-
-  AppColors appColors = AppColors();
-
-  static const LatLng sourceLocation = LatLng(48.866667, 2.333333);
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-        top: false,
-        bottom: false,
-        child: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            backgroundColor: appColors.appMediumColor,
-            body: Column(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      height: ResponsiveFlutter.of(context).moderateScale(45),
-                      width: double.infinity,
-                      alignment: Alignment.bottomCenter,
-                      padding: EdgeInsets.symmetric(
-                        vertical: ResponsiveFlutter.of(context).moderateScale(20),
-                        horizontal: ResponsiveFlutter.of(context).moderateScale(20),
-                      ),
-                      decoration: BoxDecoration(
-                        color: appColors.appDarkColor,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(ResponsiveFlutter.of(context).moderateScale(0)),
-                          bottomRight: Radius.circular(ResponsiveFlutter.of(context).moderateScale(0)),
-                        ),
-                      ),
+  Widget build(BuildContext context) => Scaffold(
 
-                    ),
-                    commonAppBar(
-                      appBarHeight:ResponsiveFlutter.of(context).moderateScale(45),
-                      iconsize: ResponsiveFlutter.of(context).moderateScale(50),
-                      editStack: StackFit.expand,
-                      textSize: ResponsiveFlutter.of(context).fontSize(2.7),
-                      editPadding: EdgeInsets.only(
-                        bottom: ResponsiveFlutter.of(context).moderateScale(0),
-                        left: ResponsiveFlutter.of(context).moderateScale(0),
-                        right: ResponsiveFlutter.of(context).moderateScale(0),
-                      ),
-                      context: context,
-                      backArrow: true,
-                      title: Strings.dummyText39,
-                      appBarColor: appColors.appDarkColor,
-                      search: true,
-                      searchClick: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SearchLocation(),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                Expanded(
-
-                    child: GoogleMap(initialCameraPosition: CameraPosition(target: sourceLocation,zoom: 14.5,))
-                )
-              ],
-            ),
+    body: Chat(
+      theme: DarkChatTheme(),
+      messages: _messages,
+      onAttachmentPressed: _handleAttachmentPressed,
+      onMessageTap: _handleMessageTap,
+      onPreviewDataFetched: _handlePreviewDataFetched,
+      onSendPressed: _handleSendPressed,
+      showUserAvatars: true,
+      showUserNames: true,
+      user: _user,
     ),
+  );
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleAttachmentPressed() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: 144,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleImageSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Photo'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleFileSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('File'),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Cancel'),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  void _handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
     );
 
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: const Uuid().v4(),
+        mimeType: lookupMimeType(result.files.single.path!),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleImageSelection() async {
+    final result = await ImagePicker().pickImage(
+      imageQuality: 70,
+      maxWidth: 1440,
+      source: ImageSource.gallery,
+    );
+
+    if (result != null) {
+      final bytes = await result.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: const Uuid().v4(),
+        name: result.name,
+        size: bytes.length,
+        uri: result.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleMessageTap(BuildContext _, types.Message message) async {
+    if (message is types.FileMessage) {
+      var localPath = message.uri;
+
+      if (message.uri.startsWith('http')) {
+        try {
+          final index =
+          _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+          (_messages[index] as types.FileMessage).copyWith(
+            isLoading: true,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+
+          final client = http.Client();
+          final request = await client.get(Uri.parse(message.uri));
+          final bytes = request.bodyBytes;
+          final documentsDir = (await getApplicationDocumentsDirectory()).path;
+          localPath = '$documentsDir/${message.name}';
+
+          if (!File(localPath).existsSync()) {
+            final file = File(localPath);
+            await file.writeAsBytes(bytes);
+          }
+        } finally {
+          final index =
+          _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+          (_messages[index] as types.FileMessage).copyWith(
+            isLoading: null,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+        }
+      }
+
+      await OpenFilex.open(localPath);
+    }
+  }
+
+  void _handlePreviewDataFetched(
+      types.TextMessage message,
+      types.PreviewData previewData,
+      ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  void _loadMessages() async {
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    setState(() {
+      _messages = messages;
+    });
   }
 }
