7934245605db1903c110a937423906dc7b769a50
Pao7o <92690817+Pao7o@users.noreply.github.com>
2022-11-14 04:11:48 +0100

    People class for pratham :)

---
+++ lib/screens/people_screen.dart
@@ -0,0 +1,31 @@
+import "package:flutter/material.dart";
+import 'package:attendify/common/app_colors.dart';
+
+
+
+class PeopleScreen extends StatefulWidget {
+  const PeopleScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PeopleScreen> createState() => _PeopleScreenState();
+}
+
+class _PeopleScreenState extends State<PeopleScreen> {
+  AppColors appColors = AppColors();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Scaffold(
+        backgroundColor: AppColors().appMediumColor,
+        body: Stack(
+          children: [
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
