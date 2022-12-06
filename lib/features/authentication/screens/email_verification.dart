47d3b360fafb1588530f6df7f6af665f4ba3d7c5
Crucial Jun <nich.otieno@gmail.com>
2022-12-06 17:58:16 +0300

    email verification

---
+++ lib/features/authentication/screens/email_verification.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmailVerificationScreen extends StatelessWidget {
+  final String emailAddress;
+  const EmailVerificationScreen({Key? key, required this.emailAddress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
