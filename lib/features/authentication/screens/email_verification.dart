3d20d47ad328c4b6a1e0b0058ce9b6eccb460860
Crucial Jun <nich.otieno@gmail.com>
2022-12-06 18:36:22 +0300

    email verification

--- lib/features/authentication/screens/email_verification.dart
+++ lib/features/authentication/screens/email_verification.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class EmailVerificationScreen extends StatelessWidget {
+  static const String routeName = "/email_verification_screen";
   final String emailAddress;
   const EmailVerificationScreen({Key? key, required this.emailAddress})
       : super(key: key);
@@ -8,7 +10,16 @@ class EmailVerificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        children: [
+          LottieBuilder.asset("assets/lottie/sent.json", repeat: false),
+          Text("Email sent to $emailAddress please check and confirm"),
+          ElevatedButton(
+              onPressed: () {}, child: const Text("Email already confirmed")),
+          const Text("Did not receive the emai?"),
+          ElevatedButton(onPressed: () {}, child: const Text("Resend email"))
+        ],
+      ),
     );
   }
 }
